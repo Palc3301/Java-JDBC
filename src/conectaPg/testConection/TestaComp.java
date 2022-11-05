f91f202d969428ea2b72aee39a554e1a309307ad
Palc3301 <pedroarruda8@live.com>
2022-11-05 12:44:44 -0300

    Front Simulation in Test Classes.

--- src/conectaPg/testConection/TestaComp.java
+++ src/conectaPg/testConection/TestaComp.java
@@ -9,17 +9,43 @@ import java.util.Scanner;
 
 public class TestaComp {
     public static void main(String[] args) {
-        
-        //computadorInsert();
-        //computadorUpdate();
-        //computadorDelete();
-        //computadorShow();
+        int opcao = 0;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("\n1. Insere Computador\n2. Atualiza Computador\n" +
+                    "3. Deleta Computador\n4. Exibe Computadores\nOpção: ");
+            opcao = Integer.parseInt(scanner.nextLine());
+            if (opcao == 1) {
+                System.out.print("Inserir Codigo do Computador: ");
+                int codComputador = Integer.parseInt(scanner.nextLine());
+                System.out.print("Inserir Descrição do Computador: ");
+                String descricao = scanner.nextLine();
+                System.out.print("Inserir Quantidade de Computadores: ");
+                int qtd = Integer.parseInt(scanner.nextLine());
+
+                computadorInsert(codComputador, descricao, qtd);
+            } else if (opcao == 2) {
+                System.out.print("Inserir Codigo do Computador: ");
+                int codComputador = Integer.parseInt(scanner.nextLine());
+                System.out.print("Inserir Quantidade de Computadores: ");
+                int qtd = Integer.parseInt(scanner.nextLine());
+
+                computadorUpdate(codComputador, qtd);
+            } else if (opcao == 3) {
+                System.out.print("Inserir Codigo do Computador: ");
+                int codComputador = Integer.parseInt(scanner.nextLine());
+
+                computadorDelete(codComputador);
+            } else if (opcao == 4) {
+                computadorShow();
+            }
+        } while (opcao >= 1 && opcao < 5);
     }
-    public static void computadorInsert() {
+    public static void computadorInsert(int codComputador, String descricao, int qtd) {
         Computador computador = new Computador();
-        computador.setCodComputador(3);
-        computador.setQtd(20);
-        computador.setDescricao("Asus TUF F15");
+        computador.setCodComputador(codComputador);
+        computador.setQtd(qtd);
+        computador.setDescricao(descricao);
 
         ComputadorDAO computadorDAO = null;
         try {
@@ -31,10 +57,10 @@ public class TestaComp {
         }
         computadorDAO.cadastrarComputador(computador);
     }
-    public static void computadorUpdate() {
+    public static void computadorUpdate(int codComputador, int qtd) {
         Computador computador = new Computador();
-        computador.setQtd(5);
-        computador.setCodComputador(1);
+        computador.setQtd(qtd);
+        computador.setCodComputador(codComputador);
         ComputadorDAO computadorDAO = null;
         try {
             computadorDAO = new ComputadorDAO();
@@ -46,9 +72,9 @@ public class TestaComp {
         }
         computadorDAO.updateComputador(computador);
     }
-    public static void computadorDelete() {
+    public static void computadorDelete(int codComputador) {
         Computador computador = new Computador();
-        computador.setCodComputador(3301);
+        computador.setCodComputador(codComputador);
         ComputadorDAO computadorDAO = null;
         try {
             computadorDAO = new ComputadorDAO();
@@ -71,6 +97,7 @@ public class TestaComp {
         }
         List<Computador> listaShowAll = computadorDAO.selectAllcomputers();
         for(Computador e:listaShowAll) {
+            System.out.println("---------------------------------------------");
             System.out.println("COD_COMPUTADOR: " + e.getCodComputador());
             System.out.println("QTD: " + e.getQtd());
             System.out.println("DESCRICAO: " + e.getDescricao());
